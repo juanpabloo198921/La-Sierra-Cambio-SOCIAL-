3e0940293685fe0f176ea8c2f4c53a03b554ebd4
juanpabloo198921 <40009567+juanpabloo198921@users.noreply.github.com>
2018-07-26 19:27:53 -0500

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,11 @@
+var config = {
+    apiKey: "AIzaSyAw3THBR3n9vPzYhEH0vODRVlYFwe8UUfE",
+    authDomain: "la-sierra-cambio-social.firebaseapp.com",
+    databaseURL: "https://la-sierra-cambio-social.firebaseio.com",
+    projectId: "la-sierra-cambio-social",
+    storageBucket: "la-sierra-cambio-social.appspot.com",
+    messagingSenderId: "129241450571"
+  };
+  firebase.initializeApp(config);
+
+  const database = firebase.database();
