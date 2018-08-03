8e71bd1b3596ab851c43126c2a97772f14759bca
juanpabloo198921 <juanpablo198921@gmail.com>
2018-08-03 00:08:51 -0500

    asignar tour

--- js/reservartour.js
+++ js/reservartour.js
@@ -1,30 +1,50 @@
-$(document).ready(function($) {
-    $('.tour').on(
+jQuery(document).ready(function($) {
+    $('#tour').click(
         'submit',
         function(e){
             e.preventDefault();
-            var nombre = $('.nombre .apellido').val();
+            var nombre = $('.registro #nombre').val();
 
-            var apellido = $('.nombre .apellido').val();
+            var apellido = $('.registro #apellido').val();
 
-            var 
-            console.log(password, email);
-            if(!email) {
-                alert('Debe ingresar un correo');
-             }
-            else if(!password){
-                alert('Debe ingresar una contrasena');
+            var email = $('.registro #email').val();
+
+            var telefono = $('.registro #telefono').val();
+
+            var nacionalidad = $('.registro #nacionalidad').val();
+
+            var recorrido = $('.registro #recorrido').val();
+
+
+            if(!nombre) {
+                alert('Debe ingresar un nombre');
+            }else if(!apellido){
+                alert('Debe ingresar un apellido');
+            }else if(!email){
+                alert('Debe ingresar un email');
+            }else if(!telefono){
+                alert('Debe ingresar un telefono');
+
+            }else if (nacionalidad == 'Selecciona la nacionalidad') {
+                alert('Debe Seleccionar una nacionalidad');
+            }else if (recorrido == 'Selecciona el recorrido') {
+                alert(" Debe Seleccionar el recorrido");
             }
-            else {
-                firebase
-                .auth()
-                .then(function(){
-                    location.href='asignartour.html';
-                })
-                .catch(function(error) {
-                    alert('No eres bienvenido');
-                });
+             else {
+                var user_info = {
+                    nombre:$('.registro #nombre').val(),
+                    apellido:$('.registro #apellido').val(),
+                    email:$('.registro #email').val(),
+                    telefono:$('.registro #telefono').val(),
+                    nacionalidad:$('.registro #nacionalidad').val(),
+                    recorrido:$('.registro #recorrido').val(),
+              
+                
+                };
+                firebase.database().ref('user_info').push(user_info);
+                alert('guardado con exito');
+                location.href="index.html"
             }
-        }
-        );
+
+        });
 })
